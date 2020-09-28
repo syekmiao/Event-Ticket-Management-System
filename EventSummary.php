0bb9a47341c226299edb183af97c6473dd3f077f
syek miao <syek miao@syekmiao-PC>
2020-09-28 21:36:58 +0800

    new commit

---
+++ EventSummary.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Event Summary</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, 
+              shrink-to-fit=no">
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <script src="jquery/jquery.slim.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.bundle.min.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <div class="container">
+            <h1 class="my-4">Event Name
+            <small>Event Date - 22/09/2020</small>
+            </h1>
+
+            <div class="row">
+
+                <div class="col-md-4">
+                    <img class="img-fluid" src="http://placehold.it/500x500" alt="">
+                </div>
+
+                <div class="col-md-8">
+                    <h3 class="my-3">Event Description</h3>
+                    <p>Event descriptions.... What the event is about. What will happen in this event.
+                    What will participations gains while attending this event... etc. etc. </p>
+                
+                    <h3 class="my-3">Event Date and Time</h3>
+                    <ul>
+                        <li>Session 1: 22/09/2020 - 8.00a.m. ~ 12.00 p.m.</li>
+                        <li>Session 2: 22/09/2020 - 6.00p.m. ~ 10.00 p.m.</li>
+                        <li>Session 3: 23/09/2020 - 8.00a.m. ~ 12.00 p.m.</li>
+                        <li>Session 1: 23/09/2020 - 6.00p.m. ~ 10.00 p.m.</li>
+                    </ul>
+                    
+                    <select class="my-3 mmdb-select md-form">
+                        <option value="" disabled selected>Choose your session</option>
+                        <option value="1">Session 1</option>
+                        <option value="2">Session 2</option>
+                        <option value="3">Session 3</option>
+                        <option value="4">Session 4</option>
+                    </select>
+                    <input type="submit" value="Book this Event" class="btn btn-primary" >
+                </div>
+            </div>
+
+            <h3 class="my-4">Other Events</h3>
+            <div class="row">
+                <div class="col-md-3 col-sm-6 mb-4">
+                    <div class="card h-50">
+                        <a href="#"><img class="img-fluid" src="http://placehold.it/500x250" alt=""></a>
+                        <div class="card-body">
+                            <h4 class="card-title"><a href="#">Event One</a></h4>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 mb-4">
+                    <div class="card h-50">
+                        <a href="#"><img class="img-fluid" src="http://placehold.it/500x250" alt=""></a>
+                        <div class="card-body">
+                            <h4 class="card-title"><a href="#">Event Two</a></h4>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 mb-4">
+                    <div class="card h-50">
+                        <a href="#"><img class="img-fluid" src="http://placehold.it/500x250" alt=""></a>
+                        <div class="card-body">
+                            <h4 class="card-title"><a href="#">Event Three</a></h4>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 mb-4">
+                    <div class="card h-50">
+                        <a href="#"><img class="img-fluid" src="http://placehold.it/500x250" alt=""></a>
+                        <div class="card-body">
+                            <h4 class="card-title"><a href="#">Event Four</a></h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+        
+        
+    </body>
+</html>
